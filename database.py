676cc2e11c650b74ff9932fc0c76f758459922e8
Bhargav <bhargav200004@gmail.com>
2025-11-03 19:26:23 +0530

    feat: implement render featur

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -5,15 +6,20 @@ from sqlalchemy.orm import sessionmaker
 # Define the SQLite database URL.
 # This will create a file named 'movies.db' in the same directory.
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./movies.db"
+SQLALCHEMY_DATABASE_URL =  os.environ.get(
+    "DATABASE_URL",
+    "sqlite:///./movies.db"
+    )
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread" : False}
-)
+if SQLALCHEMY_DATABASE_URL.startswith("postgres"):
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+else:
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        connect_args={"check_same_thread" : False}
+    )
 
 SessionLocal = sessionmaker(autocommit = False, autoflush= False, bind = engine)
-
 Base = declarative_base()
 
 def get_db():
